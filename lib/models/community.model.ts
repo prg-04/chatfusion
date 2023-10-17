61fac4a8a9157d25f673c46e7f01251986c1e725
prg-04 <karanjaevans95@gmail.com>
2023-10-17 18:47:08 +0300

    Fix Bug

--- lib/models/community.model.ts
+++ lib/models/community.model.ts
@@ -30,6 +30,6 @@ const communitySchema = new mongoose.Schema({
   ],
 });
 
-const Community = mongoose.models.Community || mongoose.model("User", communitySchema);
+const Community = mongoose.models.Community || mongoose.model("Community", communitySchema);
 
 export default Community;
